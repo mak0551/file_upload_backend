9a34b9995f8d50c1d7c71a224107770e1009e713
mohammed ahmed khan <84978114+mak0551@users.noreply.github.com>
2025-01-22 15:52:03 +0530

    nodemon

--- index.js
+++ index.js
@@ -38,5 +38,5 @@ app.post("/upload", upload.single("file"), async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 500;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
